1be9ce56432ccbb41ecbca2e300b48019742030d
JOEY <joeycarlofrancisco@gmail.com>
2019-10-04 18:58:23 +0800

    integrated back4app

--- app/src/main/java/com/example/devicemanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/devicemanagementsystem/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.devicemanagementsystem;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.parse.Parse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeParse();
+
+    }
+
+    private void initializeParse() {
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
     }
 }
